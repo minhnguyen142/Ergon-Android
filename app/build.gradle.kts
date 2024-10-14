10ec769375a3ed19ae958cc7b3bb5767ac2253e9
Minh Nguyễn <minhnguyen13864@gmail.com>
2024-10-14 18:12:05 +0700

    Fix Conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -32,11 +33,15 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.android.material:material:1.10.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.storage)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

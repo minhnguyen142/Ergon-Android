bfab493ab0b5893a2b6478fc2dff0d7dcca8a458
Android <lamngu0904@gmail.com>
2024-10-29 21:14:57 +0700

    activity pdf

--- app/src/main/java/com/example/project/BookDetail.java
+++ app/src/main/java/com/example/project/BookDetail.java
@@ -1,22 +1,21 @@
 package com.example.project;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.bumptech.glide.Glide;
-import com.example.project.R;
 import com.example.project.model.Book;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.example.project.ui.PdfViewerActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
 public class BookDetail extends AppCompatActivity {
@@ -25,8 +24,7 @@ public class BookDetail extends AppCompatActivity {
     private TextView bookTitleDetail, bookGenreDetail, bookRatingDetail, bookPriceDetail;
     private Button readButton, addLibraryButton;
     private DatabaseReference databaseReference;
-     // Thêm
-
+    private String userId; // Khai báo userId
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +40,9 @@ public class BookDetail extends AppCompatActivity {
         addLibraryButton = findViewById(R.id.addLibrary);
         btnback = findViewById(R.id.IconCLose);
 
+        // Giả sử bạn đã lưu userId khi đăng nhập
+        userId = getIntent().getStringExtra("user_id");
+
         Intent intent = getIntent();
         String bookImage = intent.getStringExtra("book_image");
 
@@ -56,10 +57,9 @@ public class BookDetail extends AppCompatActivity {
         btnback.setOnClickListener(v -> {
             finish();
         });
-
     }
 
-    private void fetchBookDetailsFromFirebase(String bookImage) { // Thêm
+    private void fetchBookDetailsFromFirebase(String bookImage) {
         databaseReference = FirebaseDatabase.getInstance().getReference("books");
         databaseReference.orderByChild("coverUrl").equalTo(bookImage).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -72,6 +72,15 @@ public class BookDetail extends AppCompatActivity {
                             bookGenreDetail.setText(book.getGenre());
                             bookRatingDetail.setText("5.0 (10 Nhận xét)");
                             bookPriceDetail.setText("Miễn phí - Bạn có thể thêm vào thư viện và đọc trọn vẹn cuốn sách miễn phí");
+
+                            Intent readIntent = new Intent(BookDetail.this, PdfViewerActivity.class);
+                            readIntent.putExtra("pdfUrl", book.getPdfUrl());
+                            readIntent.putExtra("content", book.getContent());
+
+                            readButton.setOnClickListener(v -> {
+                                startActivity(readIntent);
+//                                addToReadingHistory(book);
+                            });
                         }
                     }
                 }
@@ -82,4 +91,24 @@ public class BookDetail extends AppCompatActivity {
             }
         });
     }
+
+//    private void addToReadingHistory(Book book) {
+//        DatabaseReference historyRef = FirebaseDatabase.getInstance().getReference("users").child(userId).child("readingHistory");
+//        String historyId = historyRef.push().getKey();
+//
+//
+//        ReadingHistory readingHistory = new ReadingHistory(
+//                historyId,
+//                book.getTitle(),
+//                book.getAuthor(),
+//                System.currentTimeMillis()
+//        );
+//
+//        // Lưu vào Firebase
+//        if (historyId != null) {
+//            historyRef.child(historyId).setValue(readingHistory).addOnCompleteListener(task -> {
+//                if (task.isSuccessful()) {} else {}
+//            });
+//        }
+//    }
 }

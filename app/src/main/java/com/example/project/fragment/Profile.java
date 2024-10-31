b3e8c65d596c3f9b09ee1e09f7c8183eee5a6242
Android <lamngu0904@gmail.com>
2024-10-31 17:44:46 +0700

    fix library

--- app/src/main/java/com/example/project/fragment/Profile.java
+++ app/src/main/java/com/example/project/fragment/Profile.java
@@ -1,23 +1,42 @@
 package com.example.project.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.project.ui.History;
 import com.example.project.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Profile extends Fragment {
+    private TextView tvLib;
 
+    @SuppressLint("MissingInflatedId")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        tvLib = view.findViewById(R.id.tv_Lib);
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        String userId = sharedPreferences.getString("user_id", null);
+
+        if (userId != null) {
+            CountBook(userId, tvLib); // Gọi hàm CountBook để đếm sách
+        }
 
         LinearLayout linearHistory = view.findViewById(R.id.linear_History);
         linearHistory.setOnClickListener(v -> {
@@ -26,5 +45,22 @@ public class Profile extends Fragment {
         });
 
         return view;
+    }
+
+    public void CountBook(String userId, TextView tvLib) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users").child(userId).child("library");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int count = (int) snapshot.getChildrenCount();
+                tvLib.setText(count+" cuốn sách");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Xử lý khi có lỗi xảy ra
+            }
+        });
     }
 }
